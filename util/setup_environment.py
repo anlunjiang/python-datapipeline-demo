9d266795c9443fb7fd0f403d7edde4dc77480dea
anlunjiang <anlunjiang@outlook.com>
2022-04-17 00:41:41 +0100

    rename utils to util

---
+++ util/setup_environment.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+from schemas.connection import Database
+
+
+def setup_db_environment():
+    os.environ["DB_USER"] = "root"
+    os.environ["DB_PWD"] = "1234"
+    os.environ["DB_HOST"] = "localhost"
+    os.environ["DB_SCHEMA"] = "demo"
+    os.environ["DB_PORT"] = "61782"
+    os.environ["DB_NAME"] = "demo"
+
+
+def create_database():
+    db = Database()
+    db.execute(["CREATE DATABASE IF NOT EXISTS test_db"])
+
+
+def run_mysql_docker_container():
+    subprocess.check_output("docker-compose up -d", shell=True)
