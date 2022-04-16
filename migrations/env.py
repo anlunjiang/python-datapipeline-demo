84a92807758a225f01ef8a8a726f6f7f100eeb4a
anlunjiang <anlunjiang@outlook.com>
2022-04-17 00:40:58 +0100

    init alembic

--- migrations/env.py
+++ migrations/env.py
@@ -1,10 +1,13 @@
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+from schemas import internal
+from schemas.connection import Database
+from util.setup_environment import setup_db_environment
+
 config = context.config
 
 # Interpret the config file for Python logging.
@@ -16,7 +19,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+target_metadata = internal.Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -55,13 +60,8 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
+    setup_db_environment()
+    with Database().engine.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
